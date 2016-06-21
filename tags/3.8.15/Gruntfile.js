9783d57c5e1166aaa05f597bca707f65b1866d8f
jorbin <jorbin@602fd350-edb4-49c9-b593-d223f7449a82>
2016-06-21 18:00:26 +0000

    Tag 3.8.15
    git-svn-id: https://develop.svn.wordpress.org@37844 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tags/3.8.15/Gruntfile.js
@@ -0,0 +1,409 @@
+/* jshint node:true */
+module.exports = function(grunt) {
+	var path = require('path'),
+		SOURCE_DIR = 'src/',
+		BUILD_DIR = 'build/';
+
+	// Load tasks.
+	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
+
+	// Project configuration.
+	grunt.initConfig({
+		clean: {
+			all: [BUILD_DIR],
+			dynamic: {
+				dot: true,
+				expand: true,
+				cwd: BUILD_DIR,
+				src: []
+			},
+			tinymce: {
+				src: [
+					'<%= concat.tinymce.dest %>',
+					BUILD_DIR + 'wp-includes/js/tinymce/wp-tinymce-schema.min.js',
+					BUILD_DIR + 'wp-includes/js/tinymce/mark_loaded.js'
+				]
+			},
+			qunit: ['tests/qunit/compiled.html']
+		},
+		copy: {
+			files: {
+				files: [
+					{
+						dot: true,
+						expand: true,
+						cwd: SOURCE_DIR,
+						src: [
+							'**',
+							'!**/.{svn,git}/**', // Ignore version control directories.
+							'!wp-includes/version.php' // Exclude version.php
+						],
+						dest: BUILD_DIR
+					},
+					{
+						src: 'wp-config-sample.php',
+						dest: BUILD_DIR
+					}
+				]
+			},
+			version: {
+				options: {
+					processContent: function( src ) {
+						return src.replace( /^(\$wp_version.+?)-src';/m, '$1\';' );
+					}
+				},
+				files: [
+					{
+						src: SOURCE_DIR + 'wp-includes/version.php',
+						dest: BUILD_DIR + 'wp-includes/version.php'
+					}
+				]
+			},
+			dynamic: {
+				dot: true,
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				src: []
+			},
+			qunit: {
+				src: 'tests/qunit/index.html',
+				dest: 'tests/qunit/compiled.html',
+				options: {
+					processContent: function( src ) {
+						return src.replace( /([^\.])*\.\.\/src/ig , '/../build' );
+					}
+				}
+			}
+		},
+		sass: {
+			colors: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '.css',
+				src: ['wp-admin/css/colors/*/colors.scss'],
+				options: {
+					outputStyle: 'expanded'
+				}
+			}
+		},
+		cssmin: {
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '.min.css',
+				src: [
+					'wp-admin/css/*.css',
+					'wp-includes/css/*.css',
+					// Exceptions
+					'!wp-admin/css/theme.css', // Temporary file
+					'!wp-admin/css/farbtastic.css'
+				]
+			},
+			rtl: {
+				expand: true,
+				cwd: BUILD_DIR,
+				dest: BUILD_DIR,
+				ext: '.min.css',
+				src: [
+					'wp-admin/css/*-rtl.css',
+					'wp-includes/css/*-rtl.css'
+				]
+			},
+			colors: {
+				expand: true,
+				cwd: BUILD_DIR,
+				dest: BUILD_DIR,
+				ext: '.min.css',
+				src: [
+					'wp-admin/css/colors/*/*.css'
+				]
+			}
+		},
+		cssjanus: {
+			core: {
+				options: {
+					swapLtrRtlInUrl: false
+				},
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: [
+					'wp-admin/css/*.css',
+					'wp-includes/css/*.css'
+				]
+			},
+			colors: {
+				options: {
+					processContent: function( src ) {
+						return src.replace( /([^/]+)\.css/gi, '$1-rtl.css' );
+					}
+				},
+				expand: true,
+				cwd: BUILD_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: [
+					'wp-admin/css/colors/*/colors.css'
+				]
+			},
+			dynamic: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: []
+			}
+		},
+		jshint: {
+			options: grunt.file.readJSON('.jshintrc'),
+			grunt: {
+				src: ['Gruntfile.js']
+			},
+			tests: {
+				src: [
+					'tests/qunit/**/*.js',
+					'!tests/qunit/vendor/qunit.js'
+				],
+				options: grunt.file.readJSON('tests/qunit/.jshintrc')
+			},
+			themes: {
+				expand: true,
+				cwd: SOURCE_DIR + 'wp-content/themes',
+				src: [
+					'**/*.js',
+					'!twenty{eleven,twelve,thirteen}/**',
+					// Third party scripts
+					'!twentyfourteen/js/html5.js'
+				]
+			},
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				src: [
+					'wp-admin/js/*.js',
+					'wp-includes/js/*.js',
+					// WordPress scripts inside directories
+					'wp-includes/js/jquery/jquery.table-hotkeys.js',
+					'wp-includes/js/mediaelement/wp-mediaelement.js',
+					'wp-includes/js/plupload/handlers.js',
+					'wp-includes/js/plupload/wp-plupload.js',
+					'wp-includes/js/tinymce/plugins/wordpress/editor_plugin_src.js',
+					'wp-includes/js/tinymce/plugins/wp*/editor_plugin_src.js',
+					'wp-includes/js/tinymce/mark_loaded_src.js',
+					// Third party scripts
+					'!wp-admin/js/farbtastic.js',
+					'!wp-admin/js/iris.min.js',
+					'!wp-includes/js/backbone.min.js',
+					'!wp-includes/js/swfobject.js',
+					'!wp-includes/js/underscore.min.js',
+					'!wp-includes/js/zxcvbn.min.js',
+					'!wp-includes/js/colorpicker.js',
+					'!wp-includes/js/hoverIntent.js',
+					'!wp-includes/js/json2.js',
+					'!wp-includes/js/tw-sack.js'
+				],
+				// Remove once other JSHint errors are resolved
+				options: {
+					curly: false,
+					eqeqeq: false
+				},
+				// Limit JSHint's run to a single specified file
+				//     grunt jshint:core --file=filename.js
+				filter: function( filepath ) {
+					var index, file = grunt.option( 'file' );
+
+					// Don't filter when no target file is specified
+					if ( ! file ) {
+						return true;
+					}
+
+					// Normalize filepath for Windows
+					filepath = filepath.replace( /\\/g, '/' );
+					index = filepath.lastIndexOf( '/' + file );
+
+					// Match only the filename passed from cli
+					if ( filepath === file || ( -1 !== index && index === filepath.length - ( file.length + 1 ) ) ) {
+						return true;
+					}
+
+					return false;
+				}
+			}
+		},
+		qunit: {
+			files: ['tests/qunit/**/*.html']
+		},
+		phpunit: {
+			'default': {
+				cmd: 'phpunit',
+				args: ['-c', 'phpunit.xml.dist']
+			},
+			ajax: {
+				cmd: 'phpunit',
+				args: ['-c', 'phpunit.xml.dist', '--group', 'ajax']
+			},
+			multisite: {
+				cmd: 'phpunit',
+				args: ['-c', 'tests/phpunit/multisite.xml']
+			}
+		},
+		uglify: {
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '.min.js',
+				src: [
+					'wp-admin/js/*.js',
+					'wp-includes/js/*.js',
+					'wp-includes/js/plupload/handlers.js',
+					'wp-includes/js/plupload/wp-plupload.js',
+					'wp-includes/js/tinymce/plugins/wp*/js/*.js',
+					'wp-includes/js/tinymce/wp-tinymce-schema.js',
+					// Exceptions
+					'!wp-admin/js/custom-header.js', // Why? We should minify this.
+					'!wp-admin/js/farbtastic.js',
+					'!wp-admin/js/iris.min.js',
+					'!wp-includes/js/backbone.min.js',
+					'!wp-includes/js/swfobject.js',
+					'!wp-includes/js/underscore.min.js',
+					'!wp-includes/js/zxcvbn.min.js',
+					// Hard-coded in editimage.html
+					'!wp-includes/js/tinymce/plugins/wpeditimage/js/editimage.js'
+				]
+			},
+			tinymce: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				src: [
+					'wp-includes/js/tinymce/plugins/wordpress/editor_plugin_src.js',
+					'wp-includes/js/tinymce/plugins/wp*/editor_plugin_src.js',
+					'wp-includes/js/tinymce/mark_loaded_src.js'
+				],
+				// TinyMCE plugins use a nonstandard naming scheme: plugin files are named
+				// `editor_plugin_src.js`, and are compressed into `editor_plugin.js`.
+				rename: function(destBase, destPath) {
+					destPath = destPath.replace(/_src.js$/, '.js');
+					return path.join(destBase || '', destPath);
+				}
+			}
+		},
+		concat: {
+			tinymce: {
+				options: {
+					separator: '\n',
+					process: function( src, filepath ) {
+						return '// Source: ' + filepath.replace( BUILD_DIR, '' ) + '\n' + src;
+					}
+				},
+				src: [
+					BUILD_DIR + 'wp-includes/js/tinymce/tiny_mce.js',
+					BUILD_DIR + 'wp-includes/js/tinymce/wp-tinymce-schema.min.js',
+					BUILD_DIR + 'wp-includes/js/tinymce/themes/advanced/editor_template.js',
+					BUILD_DIR + 'wp-includes/js/tinymce/plugins/*/editor_plugin.js',
+					BUILD_DIR + 'wp-includes/js/tinymce/mark_loaded.js'
+				],
+				dest: BUILD_DIR + 'wp-includes/js/tinymce/wp-tinymce.js'
+			}
+		},
+		compress: {
+			tinymce: {
+				options: {
+					mode: 'gzip',
+					level: 9
+				},
+				src: '<%= concat.tinymce.dest %>',
+				dest: BUILD_DIR + 'wp-includes/js/tinymce/wp-tinymce.js.gz'
+			}
+		},
+		watch: {
+			all: {
+				files: [
+					SOURCE_DIR + '**',
+					// Ignore version control directories.
+					'!' + SOURCE_DIR + '**/.{svn,git}/**'
+				],
+				tasks: ['clean:dynamic', 'copy:dynamic'],
+				options: {
+					dot: true,
+					spawn: false,
+					interval: 2000
+				}
+			},
+			colors: {
+				files: [SOURCE_DIR + 'wp-admin/css/colors/**'],
+				tasks: ['sass:colors']
+			},
+			rtl: {
+				files: [
+					SOURCE_DIR + 'wp-admin/css/*.css',
+					SOURCE_DIR + 'wp-includes/css/*.css'
+				],
+				tasks: ['cssjanus:dynamic'],
+				options: {
+					spawn: false,
+					interval: 2000
+				}
+			},
+			test: {
+				files: ['tests/qunit/**'],
+				tasks: ['qunit']
+			}
+		}
+	});
+
+	// Register tasks.
+
+	// Copy task.
+	grunt.registerTask('copy:all', ['copy:files', 'copy:version']);
+
+	// RTL task.
+	grunt.registerTask('rtl', ['cssjanus:core', 'cssjanus:colors']);
+
+	// Color schemes task.
+	grunt.registerTask('colors', ['sass:colors']);
+
+	// Build task.
+	grunt.registerTask('build', ['clean:all', 'copy:all', 'cssmin:core', 'colors', 'rtl', 'cssmin:rtl', 'cssmin:colors',
+		'uglify:core', 'uglify:tinymce', 'concat:tinymce', 'compress:tinymce', 'clean:tinymce']);
+
+	// Testing tasks.
+	grunt.registerMultiTask('phpunit', 'Runs PHPUnit tests, including the ajax and multisite tests.', function() {
+		grunt.util.spawn({
+			cmd: this.data.cmd,
+			args: this.data.args,
+			opts: {stdio: 'inherit'}
+		}, this.async());
+	});
+
+	grunt.registerTask('qunit:compiled', 'Runs QUnit tests on compiled as well as uncompiled scripts.',
+		['build', 'copy:qunit', 'qunit']);
+	grunt.registerTask('test', 'Runs all QUnit and PHPUnit tasks.', ['qunit:compiled', 'phpunit']);
+
+	// Default task.
+	grunt.registerTask('default', ['build']);
+
+	// Add a listener to the watch task.
+	//
+	// On `watch:all`, automatically updates the `copy:dynamic` and `clean:dynamic`
+	// configurations so that only the changed files are updated.
+	// On `watch:rtl`, automatically updates the `cssjanus:dynamic` configuration.
+	grunt.event.on('watch', function( action, filepath, target ) {
+		if ( target !== 'all' && target !== 'rtl' ) {
+			return;
+		}
+
+		var relativePath = path.relative( SOURCE_DIR, filepath ),
+			cleanSrc = ( action === 'deleted' ) ? [relativePath] : [],
+			copySrc = ( action === 'deleted' ) ? [] : [relativePath];
+
+		grunt.config(['clean', 'dynamic', 'src'], cleanSrc);
+		grunt.config(['copy', 'dynamic', 'src'], copySrc);
+		grunt.config(['cssjanus', 'dynamic', 'src'], copySrc);
+	});
+};
---
+++ tags/3.8.15/tests/phpunit/tests/term/getTerms.php
@@ -0,0 +1,227 @@
+<?php
+
+/**
+ * @group taxonomy
+ */
+class Tests_Term_getTerms extends WP_UnitTestCase {
+	function setUp() {
+		parent::setUp();
+		
+		_clean_term_filters();
+		wp_cache_delete( 'last_changed', 'terms' );
+	}
+
+	/**
+	 * @ticket 23326
+	 */
+	function test_get_terms_cache() {
+		global $wpdb;
+
+		$posts = $this->factory->post->create_many( 15, array( 'post_type' => 'post' ) );
+		foreach ( $posts as $post )
+			wp_set_object_terms( $post, rand_str(), 'post_tag' );
+		wp_cache_delete( 'last_changed', 'terms' );
+
+		$this->assertFalse( wp_cache_get( 'last_changed', 'terms' ) );
+
+		$num_queries = $wpdb->num_queries;
+
+		// last_changed and num_queries should bump
+		$terms = get_terms( 'post_tag' );
+		$this->assertEquals( 15, count( $terms ) );
+		$time1 = wp_cache_get( 'last_changed', 'terms' );
+		$this->assertNotEmpty( $time1 );
+		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
+
+		$num_queries = $wpdb->num_queries;
+
+		// Again. last_changed and num_queries should remain the same.
+		$terms = get_terms( 'post_tag' );
+		$this->assertEquals( 15, count( $terms ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $num_queries, $wpdb->num_queries );
+
+		$num_queries = $wpdb->num_queries;
+
+
+		// Different query. num_queries should bump, last_changed should remain the same.
+		$terms = get_terms( 'post_tag', array( 'number' => 10 ) );
+		$this->assertEquals( 10, count( $terms ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
+
+		$num_queries = $wpdb->num_queries;
+
+		// Again. last_changed and num_queries should remain the same.
+		$terms = get_terms( 'post_tag', array( 'number' => 10 ) );
+		$this->assertEquals( 10, count( $terms ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $num_queries, $wpdb->num_queries );
+
+		// Force last_changed to bump
+		wp_delete_term( $terms[0]->term_id, 'post_tag' );
+
+		$num_queries = $wpdb->num_queries;
+		$this->assertNotEquals( $time1, $time2 = wp_cache_get( 'last_changed', 'terms' ) );
+
+		// last_changed and num_queries should bump after a term is deleted
+		$terms = get_terms( 'post_tag' );
+		$this->assertEquals( 14, count( $terms ) );
+		$this->assertEquals( $time2, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
+
+		$num_queries = $wpdb->num_queries;
+
+		// Again. last_changed and num_queries should remain the same.
+		$terms = get_terms( 'post_tag' );
+		$this->assertEquals( 14, count( $terms ) );
+		$this->assertEquals( $time2, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $num_queries, $wpdb->num_queries );
+
+		// @todo Repeat with term insert and update.
+	}
+
+	/**
+	 * @ticket 23506
+	 */
+	function test_get_terms_should_allow_arbitrary_indexed_taxonomies_array() {
+		$term_id = $this->factory->tag->create();
+		$terms = get_terms( array( '111' => 'post_tag' ), array( 'hide_empty' => false ) );
+		$this->assertEquals( $term_id, reset( $terms )->term_id );
+	}
+
+	/**
+	 * @ticket 13661
+	 */
+	function test_get_terms_fields() {
+		$term_id1 = $this->factory->tag->create( array( 'slug' => 'woo', 'name' => 'WOO!' ) );
+		$term_id2 = $this->factory->tag->create( array( 'slug' => 'hoo', 'name' => 'HOO!', 'parent' => $term_id1 ) );
+
+		$terms_id_parent = get_terms( 'post_tag', array( 'hide_empty' => false, 'fields' => 'id=>parent' ) );
+		$this->assertEquals( array(
+			$term_id1 => 0,
+			$term_id2 => $term_id1
+		), $terms_id_parent );
+
+		$terms_ids = get_terms( 'post_tag', array( 'hide_empty' => false, 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1, $term_id2 ), $terms_ids );
+
+		$terms_name = get_terms( 'post_tag', array( 'hide_empty' => false, 'fields' => 'names' ) );
+		$this->assertEqualSets( array( 'WOO!', 'HOO!' ), $terms_name );
+
+		$terms_id_name = get_terms( 'post_tag', array( 'hide_empty' => false, 'fields' => 'id=>name' ) );
+		$this->assertEquals( array(
+			$term_id1 => 'WOO!',
+			$term_id2 => 'HOO!',
+		), $terms_id_name );
+
+		$terms_id_slug = get_terms( 'post_tag', array( 'hide_empty' => false, 'fields' => 'id=>slug' ) );
+		$this->assertEquals( array(
+			$term_id1 => 'woo',
+			$term_id2 => 'hoo'
+		), $terms_id_slug );
+	}
+
+ 	/**
+	 * @ticket 11823
+ 	 */
+	function test_get_terms_include_exclude() {
+		global $wpdb;
+
+		$term_id1 = $this->factory->tag->create();
+		$term_id2 = $this->factory->tag->create();
+		$inc_terms = get_terms( 'post_tag', array(
+			'include' => array( $term_id1, $term_id2 ),
+			'hide_empty' => false
+		) );
+		$this->assertEquals( array( $term_id1, $term_id2 ), wp_list_pluck( $inc_terms, 'term_id' ) );
+
+		$exc_terms = get_terms( 'post_tag', array(
+			'exclude' => array( $term_id1, $term_id2 ),
+			'hide_empty' => false
+		) );
+		$this->assertEquals( array(), wp_list_pluck( $exc_terms, 'term_id' ) );
+
+		// These should not generate query errors.
+		get_terms( 'post_tag', array( 'exclude' => array( 0 ), 'hide_empty' => false ) );
+		$this->assertEmpty( $wpdb->last_error );
+
+		get_terms( 'post_tag', array( 'exclude' => array( 'unexpected-string' ), 'hide_empty' => false ) );
+		$this->assertEmpty( $wpdb->last_error );
+
+		get_terms( 'post_tag', array( 'include' => array( 'unexpected-string' ), 'hide_empty' => false ) );
+		$this->assertEmpty( $wpdb->last_error );
+	}
+
+	/**
+	 * @ticket 25710
+	 */
+	function test_get_terms_exclude_tree() {
+
+		$term_id_uncategorized = get_option( 'default_category' );
+
+		$term_id1 = $this->factory->category->create();
+		$term_id11 = $this->factory->category->create( array( 'parent' => $term_id1 ) );
+		$term_id2 = $this->factory->category->create();
+		$term_id22 = $this->factory->category->create( array( 'parent' => $term_id2 ) );
+
+		// There's something else broken in the cache cleaning routines that leads to this having to be done manually
+		delete_option( 'category_children' );
+
+		$terms = get_terms( 'category', array(
+			'exclude' => $term_id_uncategorized,
+			'fields' => 'ids',
+			'hide_empty' => false,
+		) );
+		$this->assertEquals( array( $term_id1, $term_id11, $term_id2, $term_id22 ), $terms );
+
+		$terms = get_terms( 'category', array(
+			'fields' => 'ids',
+			'exclude_tree' => "$term_id1,$term_id_uncategorized",
+			'hide_empty' => false,
+		) );
+
+		$this->assertEquals( array( $term_id2, $term_id22 ), $terms );
+
+	}
+
+	/**
+	 * @ticket 13992
+	 */
+	function test_get_terms_search() {
+		$term_id1 = $this->factory->tag->create( array( 'slug' => 'burrito' ) );
+		$term_id2 = $this->factory->tag->create( array( 'name' => 'Wilbur' ) );
+
+		$terms = get_terms( 'post_tag', array( 'hide_empty' => false, 'search' => 'bur', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1, $term_id2 ), $terms );
+	}
+
+	function test_get_terms_like() {
+		$term_id1 = $this->factory->tag->create( array( 'name' => 'burrito', 'description' => 'This is a burrito.' ) );
+		$term_id2 = $this->factory->tag->create( array( 'name' => 'taco', 'description' => 'Burning man.' ) );
+
+		$terms = get_terms( 'post_tag', array( 'hide_empty' => false, 'name__like' => 'bur', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1 ), $terms );
+
+		$terms2 = get_terms( 'post_tag', array( 'hide_empty' => false, 'description__like' => 'bur', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1, $term_id2 ), $terms2 );
+
+		$terms3 = get_terms( 'post_tag', array( 'hide_empty' => false, 'name__like' => 'Bur', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1 ), $terms3 );
+
+		$terms4 = get_terms( 'post_tag', array( 'hide_empty' => false, 'description__like' => 'Bur', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1, $term_id2 ), $terms4 );
+
+		$terms5 = get_terms( 'post_tag', array( 'hide_empty' => false, 'name__like' => 'ENCHILADA', 'fields' => 'ids' ) );
+		$this->assertEmpty( $terms5 );
+
+		$terms6 = get_terms( 'post_tag', array( 'hide_empty' => false, 'description__like' => 'ENCHILADA', 'fields' => 'ids' ) );
+		$this->assertEmpty( $terms6 );
+
+		$terms7 = get_terms( 'post_tag', array( 'hide_empty' => false, 'name__like' => 'o', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1, $term_id2 ), $terms7 );
+
+		$terms8 = get_terms( 'post_tag', array( 'hide_empty' => false, 'description__like' => '.', 'fields' => 'ids' ) );
+		$this->assertEqualSets( array( $term_id1, $term_id2 ), $terms8 );
+	}
+}
